2aeea354beeadb2c10833b1c638276945598a99b
Billy Arante <arantebillywilson@gmail.com>
2024-10-17 22:58:26 +0800

    feat(lecture-5): ✨ add spawing of bullets

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,11 +1,21 @@
 extends Node2D
 
-# load the meteor scene
+# load the scenes
 var meteor_scene: PackedScene = load("res://scenes/meteor.tscn")
+var bullet_scene: PackedScene = load("res://scenes/bullet.tscn")
+
 
 func _on_meteor_timer_timeout() -> void:
 	# create an instance of the meteor scene
 	var meteor = meteor_scene.instantiate()
-
 	# attach it to the scene tree
 	$Meteors.add_child(meteor)
+
+
+func _on_player_bullet(pos) -> void:
+	# create an instance of the bullet scene
+	var bullet = bullet_scene.instantiate()
+	# attach it to the scene tree
+	$Bullets.add_child(bullet)
+	bullet.position = pos
+	
