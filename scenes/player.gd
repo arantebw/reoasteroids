8f5b621d4dc29ca7ddf1d3c4f3a139a901665e54
Billy Arante <arantebillywilson@gmail.com>
2024-10-17 18:32:10 +0800

    feat(lecture-2): ✨ add discussion about variables

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,13 +1,11 @@
 extends Node2D
 
+@export var SPEED := 500
+
 
 func _ready() -> void:
-	position = Vector2(0, 500)
-
+	position = Vector2(0, SPEED)
 
 func _process(delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
-	print(direction)
-	if Input.is_action_pressed("ui_down"):
-		position += Vector2(1, 0) * 50 * delta
-		$PlayerImage.rotation += 0.1 * delta
+	position += direction * SPEED * delta
