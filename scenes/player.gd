5c28048fc026ac2035eb4365fa07cdc65a4de702
Billy Arante <arantebillywilson@gmail.com>
2024-10-17 22:58:26 +0800

    feat(lecture-5): ✨ add spawing of bullets

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,11 +2,17 @@ extends CharacterBody2D
 
 @export var SPEED := 500
 
+signal bullet(pos)
+
 
 func _ready() -> void:
 	position = Vector2(540, 360)
+
 
 func _process(delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * SPEED
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("shoot"):
+		bullet.emit(position)
