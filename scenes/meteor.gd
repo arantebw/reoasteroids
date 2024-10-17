c1f1d5d2a9423077a12ffa867de40843efc1d199
Billy Arante <arantebillywilson@gmail.com>
2024-10-17 21:27:17 +0800

    feat(lecture-4): ✨ add random speed, direction, and rotation of meteor spawns

--- scenes/meteor.gd
+++ scenes/meteor.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+var speed: int
+var rotation_speed: int
+var direction_x: float
+
 
 func _ready() -> void:
 	var rng := RandomNumberGenerator.new()
@@ -9,11 +13,16 @@ func _ready() -> void:
 	var random_x = rng.randi_range(0, width)
 	var random_y = rng.randi_range(-150, -50)
 	position = Vector2(random_x, random_y)
+	
+	speed = rng.randi_range(50, 200)
+	direction_x = rng.randf_range(-1, 1)
+	rotation_speed = rng.randi_range(40, 100)
 
 
 func _process(delta: float) -> void:
 	# enable meteors to enter the viewport
-	position += Vector2(0, 1.0) * 100 * delta
+	position += Vector2(direction_x, 1.0) * speed * delta
+	rotation_degrees += rotation_speed * delta
 
 
 func _on_body_entered(body: Node2D) -> void:
