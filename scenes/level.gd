b47d38ebd9b8b05816c94d16ab39a3940cd11eec
Billy Arante <arantebillywilson@gmail.com>
2024-10-17 20:57:25 +0800

    feat(lecture-4): ✨ add spawning of meteor scenes

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+# load the meteor scene
+var meteor_scene: PackedScene = load("res://scenes/meteor.tscn")
 
-func _on_timer_timeout() -> void:
-	print('meteor')
+func _on_meteor_timer_timeout() -> void:
+	# create an instance of the meteor scene
+	var meteor = meteor_scene.instantiate()
+
+	# attach it to the scene tree
+	$Meteors.add_child(meteor)
