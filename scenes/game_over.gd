20190e7b922bd562b3bc8ea4eb226828fecb58e1
Billy Arante <arantebillywilson@gmail.com>
2024-10-18 17:03:21 +0800

    feat(lecture-8): ✨ add the scoring system

--- scenes/game_over.gd
+++ scenes/game_over.gd
@@ -2,6 +2,10 @@ extends Control
 
 var level_scene: PackedScene = load("res://scenes/level.tscn")
 
+
+func _ready() -> void:
+	$CenterContainer/VBoxContainer/ScoreLabel.text = $CenterContainer/VBoxContainer/ScoreLabel.text + str(Global.score)
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("start_new_game"):
 		get_tree().change_scene_to_packed(level_scene)
