4c98e09fa765a150d1faac0dfd74f457d446bfdb
Billy Arante <arantebillywilson@gmail.com>
2024-10-18 13:37:24 +0800

    feat(lecture-6): ✨ add tween for the bullet animation

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -3,5 +3,12 @@ extends Area2D
 @export var SPEED := 500
 
 
+func _ready() -> void:
+	# bullet animation
+	$Sprite2D.scale = Vector2(0, 0)
+	var tween = create_tween()
+	tween.tween_property($Sprite2D, 'scale', Vector2(1, 1), 0.15)
+
+
 func _process(delta: float) -> void:
 	position.y -= SPEED * delta
