ec50189c44b104e3aa2ece5b17cf954a536c92a2
Billy Arante <arantebillywilson@gmail.com>
2024-10-18 17:03:21 +0800

    feat(lecture-8): ✨ add the scoring system

--- scenes/hud.gd
+++ scenes/hud.gd
@@ -2,6 +2,12 @@ extends CanvasLayer
 
 static var image = load("res://sprites/ships/ship-a/ship-a1.png")
 
+var time_elapsed := 0
+
+
+func _ready() -> void:
+	$Score/Label.text = str(0)
+
 
 func set_health(amt):
 	for child in $Lives/HBoxContainer.get_children():
@@ -10,3 +16,9 @@ func set_health(amt):
 		var text_rect = TextureRect.new()
 		text_rect.texture = image
 		$Lives/HBoxContainer.add_child(text_rect)
+
+
+func _on_score_timer_timeout() -> void:
+	time_elapsed += 1
+	$Score/Label.text = str(time_elapsed)
+	Global.score = time_elapsed
