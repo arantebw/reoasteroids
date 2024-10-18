c54477d79d1464606eaab9e0a3852853d89d6c76
Billy Arante <arantebillywilson@gmail.com>
2024-10-18 22:16:11 +0800

    feat(lecture-9): ✨ add sound when shooting bullets

--- scenes/player.gd
+++ scenes/player.gd
@@ -17,6 +17,7 @@ func _process(_delta: float) -> void:
 	
 	if Input.is_action_just_pressed("shoot") and can_shoot:
 		bullet.emit($BulletStarPosition.global_position)
+		$BulletShootSound.play()
 		can_shoot = false
 		$CanShootTimer.start()
 
